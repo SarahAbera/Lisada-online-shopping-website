554a3cb31087e18d07fcb7f597647be52a474ec7
DavG20 <dawit201121@gmail.com>
2023-02-11 22:53:58 +0300

    back-end

--- lisada/back-end/README.md
+++
@@ -1,4 +0,0 @@
-
-# NestJS_Course
-
-<h4> simple project with auth <h4>
--- lisada/back-end/src/auth/dto/update-auth.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateUserDto } from './create-auth.dto';
-
-export class UpdateAuthDto extends PartialType(CreateUserDto) {}

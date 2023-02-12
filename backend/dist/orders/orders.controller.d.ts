2bea22958a21fbe2687a35a9c1f0b5977ea0c013
sitt-a <117744439+sitt-a@users.noreply.github.com>
2023-02-12 23:45:17 +0300

    update backend

--- backend/dist/orders/orders.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { CreateOrderDto } from "./dto/order.dto";
-import { OrdersService } from "./orders.service";
-export declare class OrdersController {
-    private readonly ordersService;
-    constructor(ordersService: OrdersService);
-    getAllOrders(createOrderDto: CreateOrderDto): Promise<import("./entities/orders.entity").Orders[]>;
-    getOrders(): void;
-}
--- backend/dist/orders/orders.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { CreateOrderDto } from "./dto/order.dto";
-import { Orders } from "./entities/orders.entity";
-export declare class OrdersService {
-    private readonly orderRepository;
-    getallOrder(): Promise<Orders[]>;
-    getOrders(createorderDto: CreateOrderDto): Promise<CreateOrderDto & Orders>;
-}

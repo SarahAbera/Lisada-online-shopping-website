20f38fb2fe4c2a9256a2064a7152767c051f34c9
SarahAbera <sarayele77@gmail.com>
2023-02-13 01:25:53 +0300

    new update

---
+++ backend/dist/clothes/dto/update-clothe.dto.d.ts
@@ -0,0 +1,10 @@
+export declare class UpdateClothDto {
+    readonly price: number;
+    description: string;
+}
+export declare class UpdateClothProfile {
+    id: number;
+}
+export declare class ProfileURL {
+    profile_url: string;
+}
